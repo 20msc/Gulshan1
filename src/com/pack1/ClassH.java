5718b4fa525f9a8780b86e0627716913e441aace
GULSHAN SINGH <20msccs17@suiit.ac.in>
2023-03-01 00:59:38 +0530

    First Commit

---
+++ src/com/pack1/ClassH.java
@@ -0,0 +1,15 @@
+package com.pack1;
+
+public class ClassH
+{
+	
+	
+	public static void main(String args[])
+	{
+	String Integer="manas is my friend";
+	//String s3=s1.replace('i','3');
+	System.out.println("s3");
+	}
+	
+
+}
---
+++ src/com/pack1/pintu.java
@@ -0,0 +1,24 @@
+package Constructor;
+
+import Constructor Class gulshan6;
+
+public class Class gulsgan6
+
+{
+	public void meth1()
+	{
+		System.out.println("gulshan 3");
+	}
+
+	public static void main(String[] args)
+	
+	{
+		System.out.println("hii");
+		ClassA aobj=new ClassA();
+		aobj.meth 1();
+		System.out.println("java is awesome");
+		// TODO Auto-generated method stub
+
+	}
+
+}
